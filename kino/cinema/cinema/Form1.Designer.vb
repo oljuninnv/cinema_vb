3de51746c70cfb2ff34d57fd1bbf4a98d61440c8
oljuninnv <93469509+oljuninnv@users.noreply.github.com>
2023-05-26 20:31:43 +0700

    Add files via upload

---
+++ kino/cinema/cinema/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(292, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(230, 41)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "КИНОТЕАТР"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(45, 109)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(225, 33)
+        Me.ComboBox1.TabIndex = 1
+        Me.ComboBox1.Text = "Название фильмов"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(276, 109)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(123, 33)
+        Me.ComboBox2.TabIndex = 2
+        Me.ComboBox2.Text = "Кинозалы"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(45, 264)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 3
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(405, 109)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(113, 33)
+        Me.ComboBox3.TabIndex = 4
+        Me.ComboBox3.Text = "Время"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(524, 109)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(121, 33)
+        Me.ComboBox4.TabIndex = 5
+        Me.ComboBox4.Text = "Цена"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(45, 197)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(187, 31)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "Занято/Не занято"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(40, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 25)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Место:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Orange
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
+
+
---
+++ kino/cinema/cinema/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder
+Imports System.IO
+Imports System.Net
+
+Public Class Form1
+    Private apiURL As String = "http://127.0.0.1:8000/test"
+    Private filmList As New List(Of String)
+    Private hallList As New List(Of String)
+    Private timeBeginList As New List(Of String)
+    Private priceList As New List(Of Decimal)
+    Private dateSeansList As New List(Of Date)
+    Private seansList As New List(Of Object)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        getDataFromAPI()
+    End Sub
+
+    Private Sub getDataFromAPI()
+        Try
+            Dim request As HttpWebRequest = WebRequest.Create(apiURL)
+            request.Method = "GET"
+            request.ContentType = "application/json"
+
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim dataStream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+
+            Dim responseFromServer As String = reader.ReadToEnd()
+            Dim seansList As List(Of Object) = JsonConvert.DeserializeObject(Of List(Of Object))(responseFromServer)
+
+
+            For Each item In seansList
+                Dim film As String = item("film")
+                Dim hall As String = item("hall")
+                Dim dateSeans As Date = item("date")
+                Dim timeBegin As DateTime = item("time_begin")
+                Dim priceSeans As String = item("price_seans")
+                Dim status As Boolean = item("status")
+
+                If Not filmList.Contains(film) Then
+                    filmList.Add(film)
+                    ComboBox1.Items.Add(film)
+                End If
+                If Not hallList.Contains(hall) Then
+                    hallList.Add(hall)
+                    ComboBox2.Items.Add(hall)
+                End If
+                If Not timeBeginList.Contains(timeBegin) Then
+                    timeBeginList.Add(timeBegin)
+                    ComboBox3.Items.Add(timeBegin)
+                End If
+                If Not priceList.Contains(priceSeans) Then
+                    priceList.Add(priceSeans)
+                    ComboBox4.Items.Add(priceSeans)
+                End If
+                If Not dateSeansList.Contains(dateSeans) Then
+                    dateSeansList.Add(dateSeans)
+                End If
+            Next
+
+            reader.Close()
+            dataStream.Close()
+            response.Close()
+
+        Catch ex As WebException
+            If ex.Status = WebExceptionStatus.ProtocolError Then
+                Dim response As HttpWebResponse = DirectCast(ex.Response, HttpWebResponse)
+                If response.StatusCode = HttpStatusCode.NotFound Then
+                    MessageBox.Show("Seans matching query does not exist.")
+                End If
+            End If
+        End Try
+    End Sub
+
+    Private Sub cbFilm_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        filterData()
+    End Sub
+
+    Private Sub dtpDate_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        filterData()
+    End Sub
+
+    Private Sub cbTimeBegin_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        filterData()
+    End Sub
+
+    Private Sub cbPrice_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox4.SelectedIndexChanged
+        filterData()
+    End Sub
+
+    Private Sub filterData()
+        Dim selectedFilm As String = ComboBox1.Text
+        Dim selectedDate As Date = DateTimePicker1.Value.Date
+        Dim selectedTimeBegin As String = ComboBox3.Text
+        Dim selectedPrice As Decimal = ComboBox4.Text
+        Dim statusSeans As Boolean = False
+
+
+        If String.IsNullOrEmpty(selectedFilm) Or selectedDate = Nothing Or String.IsNullOrEmpty(selectedTimeBegin) Or selectedPrice = 0 Then
+            Return
+        End If
+
+        For Each item In seansList
+            Dim film As String = item("film")
+            Dim hall As String = item("hall")
+            Dim timeBegin As String = item("time_begin")
+            Dim priceSeans As Decimal = item("price_seans")
+            Dim dateSeans As Date = item("date_of_seans")
+            statusSeans = item("status")
+
+            If film = selectedFilm And dateSeans = selectedDate And timeBegin = selectedTimeBegin And priceSeans = selectedPrice Then
+                If statusSeans = True Then
+                    TextBox1.Text = "All seats occupied"
+                Else
+                    TextBox1.Text = "Not all seats are occupied"
+                End If
+                Return
+            End If
+        Next
+    End Sub
+End Class
